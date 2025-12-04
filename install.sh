bc3748d10d0430ce0842ec49b361292db8257f41
Uvraj-becks <169154087+Uvraj-becks@users.noreply.github.com>
2025-12-04 17:27:01 +0530

    Add some packages

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@
 
 # install packages
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-yay -S fastfetch
-yay -S matugen
+yay -S fastfetch matugen
 yay -S cava
 yay -S rofi
 yay -S rmpc
@@ -24,5 +23,27 @@ yay -S oh-my-posh
 yay -S zoxide
 yay -S pokemon-colorscripts-git
 yay -S nautilus
+yay -S flatpak
+yay -S matugen
+yay -S cava
+yay -S rofi
+yay -S rmpc
+yay -S mpd
+yay -S wlogout
+yay -S waybar
+yay -S btop
+yay -S swyanc
+yay -S vencord
+yay -S zsh
+yay -S zsh-completitions
+yay -S zsh-syntax-highlighting
+yay -S zsh-autosuggestions
+yay -S fzf
+yay -S bat
+yay -S tty-clock
+yay -S oh-my-posh
+yay -S zoxide
+yay -S pokemon-colorscripts-git
+yay -S nautilus python-pywal hyprshot python-pywalfox cbonsai spotify
 yay -S flatpak # to install vscodium, but can also modify .desktop file of vscodium installed using aur repo to 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/keyitdev/sddm-astronaut-theme/master/setup.sh)"
